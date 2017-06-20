eaca7846af3bf00364df21d6df6fc08787fae6fa
Jeremy Fosbinder <JeremyFosbinder@Gmail.com>
2017-06-19 23:38:36 -0500

    Added initial revision of the connection string page to the webserver.

---
+++ WebServer/Controllers/ConnectionStringController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebServer.Controllers
+{
+    public class ConnectionStringController : Controller
+    {
+        // GET: ConnectionString
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
